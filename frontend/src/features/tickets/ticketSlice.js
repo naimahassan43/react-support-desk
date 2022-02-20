63ee89d4220074ca42b0b8faefc956ddbd4c7853
Naima Hassan <naima.hassan434@gmail.com>
2022-02-20 20:22:53 +0000

    Fetch Tickets From Backend

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -9,6 +9,7 @@ const initialState = {
   isLoading: false,
   message: "",
 };
+
 //Create new ticket
 export const createTicket = createAsyncThunk(
   "tickets/create",
@@ -16,6 +17,26 @@ export const createTicket = createAsyncThunk(
     try {
       const token = thunkAPI.getState().auth.user.token;
       return await ticketService.createTicket(ticketData, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+//Get user ticket
+export const getTickets = createAsyncThunk(
+  "tickets/getAll",
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await ticketService.getTickets(token);
     } catch (error) {
       const message =
         (error.response &&
@@ -45,6 +66,19 @@ export const ticketSlice = createSlice({
         state.isSuccess = true;
       })
       .addCase(createTicket.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getTickets.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getTickets.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.tickets = action.payload;
+      })
+      .addCase(getTickets.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
