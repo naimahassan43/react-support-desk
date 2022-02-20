5cf410878f6c9827e69ebcae74e0be88e35392af
Naima Hassan <naima.hassan434@gmail.com>
2022-02-20 19:30:00 +0000

    Create Ticket Functionality

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -9,6 +9,25 @@ const initialState = {
   isLoading: false,
   message: "",
 };
+//Create new ticket
+export const createTicket = createAsyncThunk(
+  "tickets/create",
+  async (ticketData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await ticketService.createTicket(ticketData, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const ticketSlice = createSlice({
   name: "ticket",
@@ -16,7 +35,21 @@ export const ticketSlice = createSlice({
   reducers: {
     reset: (state) => initialState,
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createTicket.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createTicket.fulfilled, (state) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+      })
+      .addCase(createTicket.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
 });
 
 export const { reset } = ticketSlice.actions;
